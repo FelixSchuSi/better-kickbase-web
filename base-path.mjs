0cdbc604cedafcd278c20d3737afeb7447cdc4de
FelixSchuSi <felixs@fh-muenster.com>
2022-06-09 23:21:43 +0200

    Fix font loading

--- base-path.mjs
+++ base-path.mjs
@@ -1,3 +1,2 @@
 export const BASE_PATH_WITHOUT_DOMAIN = '/better-kickbase-web';
 export const SITE = 'https://felixschusi.github.io';
-export const PUBLIC_DIR = BASE_PATH_WITHOUT_DOMAIN + '/assets';
